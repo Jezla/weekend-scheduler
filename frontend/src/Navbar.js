abb002c528fa2eea9841ce3f12153cffe1cc009c
Alex Law <alexjlaww@gmail.com>
2023-01-25 15:57:48 +1100

    Modal

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -51,10 +51,10 @@ const Navbar = (props) => {
                         >
                             <Box sx={style}>
                                 <Typography id="modal-modal-title" variant="h6" component="h2">
-                                    Text in a modal
+                                    How to use this bot
                                 </Typography>
                                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                                    You don't. 
                                 </Typography>
                             </Box>
                         </Modal>
