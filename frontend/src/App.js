0944826eae336069a1924e916a219719eb291a82
Alex Law <alexjlaww@gmail.com>
2023-01-25 16:17:36 +1100

    Time to clock off

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import {DndContext, closestCenter, useSensor, useSensors, PointerSensor} from "@dnd-kit/core";
-import {arrayMove, SortableContext, verticalListSortingStrategy} from "@dnd-kit/sortable";
+import { DndContext, closestCenter, useSensor, useSensors, PointerSensor } from "@dnd-kit/core";
+import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import {
-  Button, InputLabel, MenuItem, FormControl, Select, Stack, Typography, Switch
+  Button, InputLabel, MenuItem, FormControl, Select, Stack, Typography, Switch, Alert, Snackbar
 } from '@mui/material';
 
 import SortableItem from './SortableItem';
@@ -45,7 +45,9 @@ function App() {
     new Date(2023, 3, 29),
     new Date(2023, 3, 30),
   ]);
-  const [nameList, setNameList] = useState(['asldkjaslkd asdkja', 'austin lai', 'thidaskld askdid']);
+  const [nameList, setNameList] = useState(['asldkjaslkd asdkja', 'austin lai', 'thidaskld askdid']); 
+  const [openAlert, setOpenAlert] = useState(false);
+
 
   const sensors = useSensors(
     useSensor(PointerSensor, {
@@ -56,7 +58,7 @@ function App() {
   )
 
   const handleDragEnd = (event) => {
-    const {active, over} = event;
+    const { active, over } = event;
     if (active.id !== over.id) {
       setSelectedDates((items) => {
         const activeIndex = items.indexOf(active.id);
@@ -77,32 +79,33 @@ function App() {
     setCalendarDate(date)
     if (selectedDates.length !== 6) {
       setSelectedDates([...selectedDates, date.toLocaleDateString()])
-      // TODO: Put some sort of max alert popup thingo
+      return
     }
+    setOpenAlert(true)
   }
 
-  const setDisabled = (date) => {
-    // TODO: add check for valid dates and public holidays (take from list date input)
-    if ((date.getDay() !== 0 &&
-        date.getDay() !== 6) ||
-        selectedDates.includes(date.toLocaleDateString())
-    ) {
-      return true
-    }
-    return false
+const setDisabled = (date) => {
+  // TODO: add check for valid dates and public holidays (take from list date input)
+  if ((date.getDay() !== 0 &&
+    date.getDay() !== 6) ||
+    selectedDates.includes(date.toLocaleDateString())
+  ) {
+    return true
   }
+  return false
+}
 
-  const handleSubmit = () => {
-    console.log(name, selectedDates)
-    // TODO: check for existing name
-    // TODO: probs add a confirmation popup
-  }
+const handleSubmit = () => {
+  console.log(name, selectedDates)
+  // TODO: check for existing name
+  // TODO: probs add a confirmation popup
+}
 
-  return (
-    <>
-      <NavBar></NavBar>
-      <div>
-        {/*
+return (
+  <>
+    <NavBar></NavBar>
+    <div>
+      {/*
           <TextField
             label="Name"
             variant="outlined"
@@ -110,64 +113,69 @@ function App() {
             onChange={(e) => {setName(e.target.value)}}
           />
         */}
-        <FormControl sx={{ m: 1, minWidth: 360 }} size="small">
-          <InputLabel>Select Name</InputLabel>
-          <Select
-            value={name}
-            label="Select Name"
-            onChange={(e) => setName(e.target.value)}
-          >
-            {nameList.map((name, index) => <MenuItem key={index} value={name}>{name}</MenuItem>)}
-          </Select>
-        </FormControl>
-        <FormControl>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Typography>List</Typography>
-            <Switch size="medium" checked={view} onChange={(e) => toggleView(e.target.checked)}/>
-            <Typography>Calendar</Typography>
-          </Stack>
-        </FormControl>
-      </div>
-      <div>
-        { view ?
-          <Calendar
-            onChange={date => setDates(date)}
-            value={calendarDate}
-            minDate={new Date(2023, 1)}
-            maxDate={new Date(2023, 4, 0)}
-            minDetail="month"
-            next2Label={null}
-            prev2Label={null}
-            tileDisabled={({date}) => setDisabled(date)}
-            showNeighboringMonth={false}
-          />
-          :
-          <ListView
-            selectedDates={selectedDates}
-            setSelectedDates={setSelectedDates}
-            listDates={listDates}
-          />
-        }
-        <Container className="p-3" style={{ "width": "50%" }} align="center">
-          <h3>Sort your preferences</h3>
-          <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-            <SortableContext items={selectedDates} strategy={verticalListSortingStrategy}>
-              {selectedDates.map(date => {
-                return <SortableItem key={date} id={date} items={selectedDates} setItems={setSelectedDates}/>
-              }
-              )}
-            </SortableContext>
-          </DndContext>
-        </Container>
-        <Button
-          variant="contained"
-          onClick={handleSubmit}
+      <FormControl sx={{ m: 1, minWidth: 360 }} size="small">
+        <InputLabel>Select Name</InputLabel>
+        <Select
+          value={name}
+          label="Select Name"
+          onChange={(e) => setName(e.target.value)}
         >
-          Submit
-        </Button>
-      </div>
-    </>
-  );
+          {nameList.map((name, index) => <MenuItem key={index} value={name}>{name}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <FormControl>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Typography>List</Typography>
+          <Switch size="medium" checked={view} onChange={(e) => toggleView(e.target.checked)} />
+          <Typography>Calendar</Typography>
+        </Stack>
+      </FormControl>
+    </div>
+    <div>
+      {view ?
+        <Calendar
+          onChange={date => setDates(date)}
+          value={calendarDate}
+          minDate={new Date(2023, 1)}
+          maxDate={new Date(2023, 4, 0)}
+          minDetail="month"
+          next2Label={null}
+          prev2Label={null}
+          tileDisabled={({ date }) => setDisabled(date)}
+          showNeighboringMonth={false}
+        />
+        :
+        <ListView
+          selectedDates={selectedDates}
+          setSelectedDates={setSelectedDates}
+          listDates={listDates}
+        />
+      }
+      <Container className="p-3" style={{ "width": "50%" }} align="center">
+        <h3>Sort your preferences</h3>
+        <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+          <SortableContext items={selectedDates} strategy={verticalListSortingStrategy}>
+            {selectedDates.map(date => {
+              return <SortableItem key={date} id={date} items={selectedDates} setItems={setSelectedDates} />
+            }
+            )}
+          </SortableContext>
+        </DndContext>
+      </Container>
+      <Button
+        variant="contained"
+        onClick={handleSubmit}
+      >
+        Submit
+      </Button>
+    </div>
+    <Snackbar open={openAlert} autoHideDuration={6000} onClose={() => setOpenAlert(false)}>
+        <Alert onClose={() => setOpenAlert(false)} severity="error" sx={{ width: '100%' }}>
+          This is a alex message!
+        </Alert>
+      </Snackbar>
+  </>
+);
 }
 
 export default App;
