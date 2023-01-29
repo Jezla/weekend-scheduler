a3438aa78af21306704e075fa55f08a901e69387
Alex Law <alexjlaww@gmail.com>
2023-01-30 10:02:17 +1100

    Working on styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,4 @@
+import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import { DndContext, closestCenter, useSensor, useSensors, PointerSensor } from "@dnd-kit/core";
@@ -5,7 +6,7 @@ import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-ki
 import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import {
-  Button, InputLabel, MenuItem, FormControl, Select, Stack, Typography, Switch, Alert, Snackbar
+  Button, InputLabel, MenuItem, FormControl, Select, Stack, Typography, Switch, Alert, Snackbar, Grid, Box
 } from '@mui/material';
 
 import SortableItem from '../components/SortableItem';
@@ -82,97 +83,95 @@ function Home() {
     setOpenAlert(true)
   }
 
-const setDisabled = (date) => {
-  // TODO: add check for valid dates and public holidays (take from list date input)
-  if ((date.getDay() !== 0 &&
-    date.getDay() !== 6) ||
-    selectedDates.includes(date.toLocaleDateString())
-  ) {
-    return true
+  const setDisabled = (date) => {
+    // TODO: add check for valid dates and public holidays (take from list date input)
+    if ((date.getDay() !== 0 &&
+      date.getDay() !== 6) ||
+      selectedDates.includes(date.toLocaleDateString())
+    ) {
+      return true
+    }
+    return false
   }
-  return false
-}
 
-const handleSubmit = () => {
-  console.log(name, selectedDates)
-  // TODO: check for existing name
-  // TODO: probs add a confirmation popup
-}
+  const handleSubmit = () => {
+    console.log(name, selectedDates)
+    // TODO: check for existing name
+    // TODO: probs add a confirmation popup
+  }
 
-return (
-  <>
-    <div>
-      {/*
-          <TextField
-            label="Name"
-            variant="outlined"
+  return (
+    <>
+      <Grid container
+        direction="row"
+        justifyContent="center"
+        alignItems="center">
+        <FormControl sx={{ m: 1, minWidth: 360 }}>
+          <InputLabel>Select Name</InputLabel>
+          <Select
             value={name}
-            onChange={(e) => {setName(e.target.value)}}
+            label="Select Name"
+            onChange={(e) => setName(e.target.value)}
+          >
+            {nameList.map((name, index) => <MenuItem key={index} value={name}>{name}</MenuItem>)}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Typography>List</Typography>
+            <Switch size="medium" checked={view} onChange={(e) => toggleView(e.target.checked)} />
+            <Typography>Calendar</Typography>
+          </Stack>
+        </FormControl>
+      </Grid>
+      <div>
+        {view ?
+          <Calendar
+            onChange={date => setDates(date)}
+            value={calendarDate}
+            minDate={new Date(2023, 1)}
+            maxDate={new Date(2023, 4, 0)}
+            minDetail="month"
+            next2Label={null}
+            prev2Label={null}
+            tileDisabled={({ date }) => setDisabled(date)}
+            showNeighboringMonth={false}
           />
-        */}
-      <FormControl sx={{ m: 1, minWidth: 360 }} size="small">
-        <InputLabel>Select Name</InputLabel>
-        <Select
-          value={name}
-          label="Select Name"
-          onChange={(e) => setName(e.target.value)}
-        >
-          {nameList.map((name, index) => <MenuItem key={index} value={name}>{name}</MenuItem>)}
-        </Select>
-      </FormControl>
-      <FormControl>
-        <Stack direction="row" spacing={1} alignItems="center">
-          <Typography>List</Typography>
-          <Switch size="medium" checked={view} onChange={(e) => toggleView(e.target.checked)} />
-          <Typography>Calendar</Typography>
-        </Stack>
-      </FormControl>
-    </div>
-    <div>
-      {view ?
-        <Calendar
-          onChange={date => setDates(date)}
-          value={calendarDate}
-          minDate={new Date(2023, 1)}
-          maxDate={new Date(2023, 4, 0)}
-          minDetail="month"
-          next2Label={null}
-          prev2Label={null}
-          tileDisabled={({ date }) => setDisabled(date)}
-          showNeighboringMonth={false}
-        />
-        :
-        <ListView
-          selectedDates={selectedDates}
-          setSelectedDates={setSelectedDates}
-          listDates={listDates}
-        />
-      }
-      <Container className="p-3" style={{ "width": "50%" }} align="center">
-        <h3>Sort your preferences</h3>
-        <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-          <SortableContext items={selectedDates} strategy={verticalListSortingStrategy}>
-            {selectedDates.map(date => {
-              return <SortableItem key={date} id={date} items={selectedDates} setItems={setSelectedDates} />
-            }
-            )}
-          </SortableContext>
-        </DndContext>
-      </Container>
-      <Button
-        variant="contained"
-        onClick={handleSubmit}
-      >
-        Submit
-      </Button>
-    </div>
-    <Snackbar open={openAlert} autoHideDuration={6000} onClose={() => setOpenAlert(false)}>
+          :
+          <ListView
+            selectedDates={selectedDates}
+            setSelectedDates={setSelectedDates}
+            listDates={listDates}
+          />
+        }
+        <Container className="p-3" style={{ "width": "50%" }} align="center">
+          <h3>Sort your preferences</h3>
+          <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+            <SortableContext items={selectedDates} strategy={verticalListSortingStrategy}>
+              {selectedDates.map(date => {
+                return <SortableItem key={date} id={date} items={selectedDates} setItems={setSelectedDates} />
+              }
+              )}
+            </SortableContext>
+          </DndContext>
+        </Container>
+        <Box textAlign='center'>
+          <Button variant="contained"
+            onClick={handleSubmit}
+            alignItems="center">
+            Submit
+          </Button>
+        </Box>
+      </div>
+
+
+      <Snackbar open={openAlert} autoHideDuration={6000} onClose={() => setOpenAlert(false)}>
         <Alert onClose={() => setOpenAlert(false)} severity="error" sx={{ width: '100%' }}>
           This is a alex message!
         </Alert>
       </Snackbar>
-  </>
-);
+    </>
+  );
 }
 
 export default Home;
