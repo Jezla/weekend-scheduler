e97705c4eb68faef1e5a18588da24c2fe487d6ac
Gt3443 <gabrielmadler@gmail.com>
2023-01-30 10:59:38 +1100

    Removed Manager.py

--- backend/Manager.py
+++
@@ -1,8 +0,0 @@
-from SRE import *
-
-class Manager(SRE):
-    '''
-    Class represents a manager for a weekend shift
-    A manager has the same attributes as an SRE, which means they can also have preferences for which shifts
-    they would like to supervise
-    '''
