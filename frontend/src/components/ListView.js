05f9ce39df9c10cc563486d9e433fd2cf9ba1d1f
Alex Law <alexjlaww@gmail.com>
2023-01-31 10:14:22 +1100

    Alerts fixed

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Snackbar, Alert} from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 
 const ListView = (props) => {
   const [monthList1, setMonthList1] = React.useState([]);
   const [monthList2, setMonthList2] = React.useState([]);
   const [monthList3, setMonthList3] = React.useState([]);
+  const [openAlert, setOpenAlert] = React.useState(false);
 
 
   React.useEffect(() => {
     const q = 0 // TODO: Un-hardcode this (get current quarter)
 
     const formatMonthList = (q, i) => {
-      const quarters = [[1,2,3],[4,5,6],[7,8,9],[10,11,0]];
+      const quarters = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 0]];
       const filteredMonth = [...props.listDates].filter(date => date.getMonth() === quarters[q][i]);
       if (filteredMonth[0].getDay() === 'Sunday') {
         filteredMonth.unshift(null)
@@ -23,7 +24,7 @@ const ListView = (props) => {
       }
 
       const splitDays = filteredMonth.reduce((days, date, index) => {
-        return (index % 2 === 0 ? days.push([date]) : days[days.length-1].push(date)) && days;
+        return (index % 2 === 0 ? days.push([date]) : days[days.length - 1].push(date)) && days;
       }, []);
 
       return splitDays;
@@ -62,7 +63,7 @@ const ListView = (props) => {
         alignItems="center"
       >
         {pair.map((date, pairIndex) => {
-          if (date === null)  {
+          if (date === null) {
             return (
               <Grid
                 key={'p' + index + 'i' + pairIndex}
@@ -81,8 +82,8 @@ const ListView = (props) => {
               minHeight={25}
               minWidth={100}
               sx={props.selectedDates.includes(date.toLocaleDateString())
-                ? {...defaultBorder, cursor: 'pointer', backgroundColor: '#ccc'}
-                : {...defaultBorder, cursor: 'pointer'}
+                ? { ...defaultBorder, cursor: 'pointer', backgroundColor: '#ccc' }
+                : { ...defaultBorder, cursor: 'pointer' }
               }
               xs={6}
               onClick={() => {
@@ -92,7 +93,7 @@ const ListView = (props) => {
                   newDates.splice(index, 1);
                   props.setSelectedDates(newDates)
                 } else if (props.selectedDates.length === 6) {
-                  // TODO: Put some sort of max alert popup thingo
+                  setOpenAlert(true)
                 } else {
                   props.setSelectedDates([...props.selectedDates, date.toLocaleDateString()])
                 }
@@ -107,139 +108,145 @@ const ListView = (props) => {
   }
 
   return (
-    <Container
-      sx={{ pt:4 }}
-    >
-      <Grid
-        container
-        spacing={2}
-        justifyContent='center'
+    <>
+      <Container
+        sx={{ pt: 4 }}
       >
         <Grid
           container
-          xs={4}
-          sx={{ pl: 4, pr: 4 }}
-          direction='column'
+          spacing={2}
+          justifyContent='center'
         >
           <Grid
             container
-            justifyContent="center"
-            alignItems="center"
+            xs={4}
+            sx={{ pl: 4, pr: 4 }}
+            direction='column'
           >
-            <Typography sx={{ fontWeight: 'bold' }}>
-              {monthList1[0] && monthList1[0][1].toLocaleString('default', { month: 'long' })}
-            </Typography>
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography sx={{ fontWeight: 'bold' }}>
+                {monthList1[0] && monthList1[0][1].toLocaleString('default', { month: 'long' })}
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid
+                minHeight={25}
+                minWidth={100}
+                sx={defaultBorder}
+                xs={6}
+              >
+                <Typography sx={{ fontWeight: 'bold' }}>Sat</Typography>
+              </Grid>
+              <Grid
+                minHeight={25}
+                minWidth={100}
+                sx={defaultBorder}
+                xs={6}
+              >
+                <Typography sx={{ fontWeight: 'bold' }}>Sun</Typography>
+              </Grid>
+            </Grid>
+            {monthList1 && monthList1.map((pair, index) => {
+              return displayDate(pair, index);
+            })}
           </Grid>
           <Grid
             container
-            justifyContent="center"
-            alignItems="center"
+            xs={4}
+            sx={{ pl: 4, pr: 4 }}
+            direction='column'
           >
             <Grid
-              minHeight={25}
-              minWidth={100}
-              sx={defaultBorder}
-              xs={6}
+              container
+              justifyContent="center"
+              alignItems="center"
             >
-              <Typography sx={{ fontWeight: 'bold' }}>Sat</Typography>
+              <Typography sx={{ fontWeight: 'bold' }}>
+                {monthList2[0] && monthList2[0][1].toLocaleString('default', { month: 'long' })}
+              </Typography>
             </Grid>
             <Grid
-              minHeight={25}
-              minWidth={100}
-              sx={defaultBorder}
-              xs={6}
+              container
+              justifyContent="center"
+              alignItems="center"
             >
-              <Typography sx={{ fontWeight: 'bold' }}>Sun</Typography>
+              <Grid
+                minHeight={25}
+                minWidth={100}
+                sx={defaultBorder}
+                xs={6}
+              >
+                <Typography sx={{ fontWeight: 'bold' }}>Sat</Typography>
+              </Grid>
+              <Grid
+                minHeight={25}
+                minWidth={100}
+                sx={defaultBorder}
+                xs={6}
+              >
+                <Typography sx={{ fontWeight: 'bold' }}>Sun</Typography>
+              </Grid>
             </Grid>
+            {monthList2 && monthList2.map((date, index) => {
+              return displayDate(date, index);
+            })}
           </Grid>
-          {monthList1 && monthList1.map((pair, index) => {
-            return displayDate(pair, index)
-          })}
+          <Grid
+            container
+            xs={4}
+            sx={{ pl: 4, pr: 4 }}
+            direction='column'
+          >
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography sx={{ fontWeight: 'bold' }}>
+                {monthList3[0] && monthList3[0][1].toLocaleString('default', { month: 'long' })}
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid
+                minHeight={25}
+                minWidth={100}
+                sx={{ ...defaultBorder, textAlign: 'center' }}
+                xs={6}
+              >
+                <Typography sx={{ fontWeight: 'bold' }}>Sat</Typography>
+              </Grid>
+              <Grid
+                minHeight={25}
+                minWidth={100}
+                sx={{ ...defaultBorder, textAlign: 'center' }}
+                xs={6}
+              >
+                <Typography sx={{ fontWeight: 'bold' }}>Sun</Typography>
+              </Grid>
+            </Grid>
+            {monthList3 && monthList3.map((date, index) => {
+              return displayDate(date, index);
+            })}
+          </Grid>
         </Grid>
-        <Grid
-          container
-          xs={4}
-          sx={{ pl: 4, pr: 4 }}
-          direction='column'
-        >
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Typography sx={{ fontWeight: 'bold' }}>
-              {monthList2[0] && monthList2[0][1].toLocaleString('default', { month: 'long' })}
-            </Typography>
-          </Grid>
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid
-              minHeight={25}
-              minWidth={100}
-              sx={defaultBorder}
-              xs={6}
-            >
-              <Typography sx={{ fontWeight: 'bold' }}>Sat</Typography>
-            </Grid>
-            <Grid
-              minHeight={25}
-              minWidth={100}
-              sx={defaultBorder}
-              xs={6}
-            >
-              <Typography sx={{ fontWeight: 'bold' }}>Sun</Typography>
-            </Grid>
-          </Grid>
-          {monthList2 && monthList2.map((date, index) => {
-            return displayDate(date, index)
-          })}
-        </Grid>
-        <Grid
-          container
-          xs={4}
-          sx={{ pl: 4, pr: 4 }}
-          direction='column'
-        >
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Typography sx={{ fontWeight: 'bold' }}>
-              {monthList3[0] && monthList3[0][1].toLocaleString('default', { month: 'long' })}
-            </Typography>
-          </Grid>
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid
-              minHeight={25}
-              minWidth={100}
-              sx={{...defaultBorder, textAlign: 'center' }}
-              xs={6}
-            >
-              <Typography sx={{ fontWeight: 'bold' }}>Sat</Typography>
-            </Grid>
-            <Grid
-              minHeight={25}
-              minWidth={100}
-              sx={{...defaultBorder, textAlign: 'center' }}
-              xs={6}
-            >
-              <Typography sx={{ fontWeight: 'bold' }}>Sun</Typography>
-            </Grid>
-          </Grid>
-          {monthList3 && monthList3.map((date, index) => {
-            return displayDate(date, index)
-          })}
-        </Grid>
-      </Grid>
-    </Container>
+      </Container><Snackbar open={openAlert} autoHideDuration={6000} onClose={() => setOpenAlert(false)}>
+        <Alert onClose={() => setOpenAlert(false)} severity="error" sx={{ width: '100%' }}>
+          Maximum shifts selected!
+        </Alert>
+      </Snackbar>
+    </>
   )
 };
 
