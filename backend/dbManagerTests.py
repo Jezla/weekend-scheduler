64b4b15d097539d776e7e159da9c85f069323002
Robrules <bannayanr@outlook.com.au>
2023-02-09 11:47:41 +1100

    sample test case

---
+++ backend/dbManagerTests.py
@@ -0,0 +1,18 @@
+import unittest
+from dbManager import *
+from SRE import *
+
+class TestdbManager(unittest.TestCase):
+
+    def test_add_user(self):
+        dbmanager = dbManager()
+        pref = []
+        sre = SRE("id13482", "ms",pref, "michael", "islamov",1)
+        dbmanager.add_user(sre)
+        sre = dbmanager.get_user_byname("michael", "islamov")
+        self.assertEqual(sre.get_first_name(),"michael")
+        self.assertEqual(sre.get_last_name(),"islamov")
+        self.assertEqual(sre.get_id(),"id13482")
+
+if __name__ == '__main__':
+    unittest.main()
